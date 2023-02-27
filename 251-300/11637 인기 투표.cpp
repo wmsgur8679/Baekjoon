d0a21755c463b3e0988e3d962ec12e6c2f338f28
wmsgur8679 <57565691+wmsgur8679@users.noreply.github.com>
2023-02-27 22:04:07 +0900

    251-300

--- 251-300/11637 인기 투표.cpp
+++ 251-300/11637 인기 투표.cpp
@@ -1,29 +1,28 @@
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	vector <int> major, candidates;
-//	int t, ts;
-//	cin >> t;
-//	while (t--)
-//	{
-//		int totalVote = 0, maxVote = 0;
-//		cin >> ts;
-//		candidates.resize(ts);
-//		major.clear();
-//		for (int i = 0; i < ts; i++) {
-//			cin >> candidates[i];
-//			totalVote += candidates[i];
-//			maxVote = max(maxVote, candidates[i]);
-//		}
-//		for (int i = 0; i < ts; i++)
-//			if (candidates[i] == maxVote)
-//				major.push_back(i + 1);
-//		if (major.size() > 1) cout << "no winner\n";
-//		else if (totalVote / 2 < maxVote) cout << "majority winner " << major[0] << "\n";
-//		else cout << "minority winner " << major[0] << "\n";
-//	}
-//}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector <int> major, candidates;
+	int t, ts;	cin >> t;
+	while (t--)
+	{
+		int totalVote = 0, maxVote = 0;
+        cin >> ts;
+		candidates.resize(ts);
+		major.clear();
+		for (int i = 0; i < ts; i++) {
+			cin >> candidates[i];
+			totalVote += candidates[i];
+			maxVote = max(maxVote, candidates[i]);
+	}
+		for (int i = 0; i < ts; i++)
+			if (candidates[i] == maxVote)
+				major.push_back(i + 1);
+		if (major.size() > 1) cout << "no winner\n";
+		else if (totalVote / 2 < maxVote) cout << "majority winner " << major[0] << "\n";
+		else cout << "minority winner " << major[0] << "\n";
+	}
+}
--- 251-300/5585 거스름돈.cpp
+++ 251-300/5585 거스름돈.cpp
@@ -1,24 +1,24 @@
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	vector<int> m = { 500, 100, 50, 10, 5, 1 };
-//	int money, cnt =0;
-//	cin >> money;
-//	money = 1000 - money;
-//
-//	for (int i = 0; i < 6; i++) {
-//		for (;;) {
-//			if (money >= m[i]) {
-//				money -= m[i];
-//				cnt++;
-//			}
-//			else break;
-//		}
-//	}
-//	cout << cnt;
-//	
-//}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> m = { 500, 100, 50, 10, 5, 1 };
+	int money, cnt =0;
+	cin >> money;
+	money = 1000 - money;
+
+	for (int i = 0; i < 6; i++) {
+		for (;;) {
+			if (money >= m[i]) {
+				money -= m[i];
+				cnt++;
+			}
+			else break;
+		}
+	}
+	cout << cnt;
+	
+}
